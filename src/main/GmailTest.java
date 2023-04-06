5910760116e4148b3c4ba39835a25138b74eedf5
pastoraljordan <alonzojordan.pastoral@sjsu.edu>
2023-04-05 20:27:36 -0700

    Added GmailTest, can send messages

---
+++ src/main/GmailTest.java
@@ -0,0 +1,108 @@
+package main;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GmailTest {
+	// Using school email for testing, will use new email separate from school for actual sending
+	private static final String testEmail = "alonzojordan.pastoral@sjsu.edu";
+	private static final String testNum = "9253237905@txt.att.net";
+	private static final String testNum2 = "3852610378@vtext.com";
+	
+	
+	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT, GsonFactory jsonFactory)
+		      throws IOException {
+		    // Load client secrets.
+		    GoogleClientSecrets clientSecrets =
+		        GoogleClientSecrets.load(jsonFactory, new InputStreamReader(GmailTest.class.getResourceAsStream("/testClientSecrets.json")));
+
+		    // Build flow and trigger user authorization request.
+		    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+		        HTTP_TRANSPORT, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_SEND))
+		        .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+		        .setAccessType("offline")
+		        .build();
+		    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+		    //returns an authorized Credential object.
+		    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+		  }
+	
+	private void sendMessage(String subject, String msg) throws Exception {
+		
+		 final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		 GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+		 Gmail service = new Gmail.Builder(HTTP_TRANSPORT, jsonFactory, getCredentials(HTTP_TRANSPORT, jsonFactory))
+			        .setApplicationName("Test Mailer")
+			        .build();
+
+		    // Encode as MIME message
+		    Properties props = new Properties();
+		    Session session = Session.getDefaultInstance(props, null);
+		    MimeMessage email = new MimeMessage(session);
+		    email.setFrom(new InternetAddress(testEmail));
+		    email.addRecipient(javax.mail.Message.RecipientType.TO,
+		        new InternetAddress(testNum2));
+		    email.setSubject(subject);
+		    email.setText(msg);
+
+		    // Encode and wrap the MIME message into a gmail message
+		    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		    email.writeTo(buffer);
+		    byte[] rawMessageBytes = buffer.toByteArray();
+		    String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+		    Message message = new Message();
+		    message.setRaw(encodedEmail);
+
+		    try {
+		      // Create send message
+		      message = service.users().messages().send("me", message).execute();
+		      System.out.println("Message id: " + message.getId());
+		      System.out.println(message.toPrettyString());	
+		    } catch (GoogleJsonResponseException e) {
+		      // TODO(developer) - handle error appropriately
+		      GoogleJsonError error = e.getDetails();
+		      if (error.getCode() == 403) {
+		        System.err.println("Unable to send message: " + e.getDetails());
+		      } else {
+		        throw e;
+		      }
+		    }
+	}
+	
+	public static void main(String[] args) throws Exception{
+		new GmailTest().sendMessage("Project Testing :P", "Hi Anna!!\n"
+				+ "I'm Testing to see if my project is working correctly!");
+	}
+}
